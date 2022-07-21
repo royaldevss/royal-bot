60b8c0ec1112f2764a6796ec1d57dd122af289a9
Raul Rodrigues <raulrodriguesdemoraes@gmail.com>
2022-07-20 21:27:52 -0500

    [ADDED] Initial Structure

--- index.js
+++ index.js
@@ -1,2 +1,43 @@
-const dotenv = require('dotenv');
-dotenv.config();
+const fs = require('node:fs');
+const path = require('node:path');
+const { Client, Collection, Intents } = require('discord.js');
+require('dotenv').config();
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+/*
+* RODE INDIVIDUALMENTE 'node deploy-commands.js'
+* APENAS quando adicionar novo comando!
+*/
+
+client.commands = new Collection();
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	client.commands.set(command.data.name, command);
+}
+
+client.once('ready', () => {
+	console.log('Pronto!');
+});
+
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	const command = client.commands.get(interaction.commandName);
+
+	if (!command) return;
+
+	try {
+		await command.execute(interaction);
+	}
+	catch (error) {
+		console.error(error);
+		await interaction.reply({ content: 'Houve um erro ao executar esse comando!', ephemeral: true });
+	}
+});
+
+client.login(process.env.PRODTOKEN);
