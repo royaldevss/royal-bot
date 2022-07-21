e46fbfdcafdc25f9d87173ff888dcd3a8a5989d4
Raul Rodrigues <raulrodriguesdemoraes@gmail.com>
2022-07-20 21:31:39 -0500

    [FIX] Discord API Intents

--- commands/avatar.js
+++ commands/avatar.js
@@ -4,9 +4,9 @@ module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('avatar')
 		.setDescription('Veja o avatar de um usuário ou o seu próprio avatar')
-		.addUserOption(option => option.setName('target').setDescription('Visualize o Avatar de algúem')),
+		.addUserOption(option => option.setName('pessoa').setDescription('Visualize o Avatar de algúem')),
 	async execute(interaction) {
-		const user = interaction.options.getUser('target');
+		const user = interaction.options.getUser('pessoa');
 		if (user) return interaction.reply(`Avatar de ${user.username}': ${user.displayAvatarURL({ dynamic: true })}`);
 		return interaction.reply(`Seu avatar: ${interaction.user.displayAvatarURL({ dynamic: true })}`);
 	},
--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const fs = require('node:fs');
 const path = require('node:path');
-const { Client, Collection, Intents } = require('discord.js');
+const { Client, Collection, GatewayIntentBits } = require('discord.js');
 require('dotenv').config();
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 /*
 * RODE INDIVIDUALMENTE 'node deploy-commands.js'
