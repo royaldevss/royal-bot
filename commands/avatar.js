8c6e44a3f869a1b6515560f47d774c55401f3879
Raul Rodrigues <raulrodriguesdemoraes@gmail.com>
2022-07-22 12:48:36 -0500

    [NEW] embed avatar struture

--- commands/avatar.js
+++ commands/avatar.js
@@ -1,13 +1,20 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
+/* eslint-disable no-mixed-spaces-and-tabs */
+const { SlashCommandBuilder } = require('@discordjs/builders'),
+	{ EmbedBuilder } = require('discord.js'),
+	config = require('../config.json');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('avatar')
 		.setDescription('Veja o avatar de um usuário ou o seu próprio avatar')
-		.addUserOption(option => option.setName('pessoa').setDescription('Visualize o Avatar de algúem')),
+		.addUserOption(option => option.setName('usuario').setDescription('Visualize o Avatar de algúem')),
 	async execute(interaction) {
-		const user = interaction.options.getUser('pessoa');
-		if (user) return interaction.reply(`Avatar de ${user.username}: ${user.displayAvatarURL({ dynamic: true })}`);
-		return interaction.reply(`Seu avatar: ${interaction.user.displayAvatarURL({ dynamic: true })}`);
+		const user = interaction.options.getUser('usuario') || interaction.user;
+		  const embed = new EmbedBuilder()
+		  .setColor(config.botConfig.themeColor)
+		  .setTitle(`Avatar de ${user.username}#${user.discriminator}`)
+		  .setImage(user.displayAvatarURL({ format: 'png' }));
+
+		return interaction.reply({ embeds: [embed] });
 	},
 };
