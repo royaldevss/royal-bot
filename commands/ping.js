6d2a9bd93490076f3785cc3f4dc6ee1be111b5b2
Raul Rodrigues <raulrodriguesdemoraes@gmail.com>
2022-07-24 11:15:22 -0500

    [UPGRADE] Ping Command Visual

--- commands/ping.js
+++ commands/ping.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-mixed-spaces-and-tabs */
 const { SlashCommandBuilder } = require('@discordjs/builders'),
+	{ EmbedBuilder } = require('discord.js'),
 	config = require('../config.json');
 
 module.exports = {
@@ -7,7 +8,12 @@ module.exports = {
 		.setName('ping')
 		.setDescription('Veja informações sobre o ping do bot!'),
 	async execute(interaction) {
+		const { client } = interaction;
 		const sent = await interaction.reply({ content: 'Pinging...', fetchReply: true });
-		interaction.editReply(`Roundtrip latency: ${sent.createdTimestamp - interaction.createdTimestamp}ms`);
+		const embed = new EmbedBuilder()
+			.setColor(config.botConfig.themeColor)
+			.setTitle('Ping!')
+			.setDescription(`Roundtrip latency: ${sent.createdTimestamp - interaction.createdTimestamp}ms / Websocket heartbeat: ${client.ws.ping}ms.`);
+		interaction.editReply({ embeds: [embed], content:'Pronto!' });
 	},
 };
