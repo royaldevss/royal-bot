23aa97b280e3a405f4035cca4b05f9a183aca6b5
Raul Rodrigues <raulrodriguesdemoraes@gmail.com>
2022-07-24 11:31:24 -0500

    [ADDED] Ping System

--- bot.js
+++ bot.js
@@ -1,37 +1,39 @@
-const fs = require('node:fs');
-const path = require('node:path');
-const { Client, Collection, GatewayIntentBits } = require('discord.js');
-require('dotenv').config();
+module.exports = app => {
+	const fs = require('node:fs');
+	const path = require('node:path');
+	const { Client, Collection, GatewayIntentBits } = require('discord.js');
+	require('dotenv').config();
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+	const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-/*
+	/*
 * RODE INDIVIDUALMENTE 'node deploy-commands.js'
 * APENAS quando adicionar novo comando!
 */
 
-client.commands = new Collection();
-const commandsPath = path.join(__dirname, 'commands');
-const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+	client.commands = new Collection();
+	const commandsPath = path.join(__dirname, 'commands');
+	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
-for (const file of commandFiles) {
-	const filePath = path.join(commandsPath, file);
-	const command = require(filePath);
-	client.commands.set(command.data.name, command);
-}
-
-const eventsPath = path.join(__dirname, 'events');
-const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
-
-for (const file of eventFiles) {
-	const filePath = path.join(eventsPath, file);
-	const event = require(filePath);
-	if (event.once) {
-		client.once(event.name, (...args) => event.execute(...args));
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file);
+		const command = require(filePath);
+		client.commands.set(command.data.name, command);
 	}
-	else {
-		client.on(event.name, (...args) => event.execute(...args));
-	}
-}
 
-client.login(process.env.PRODTOKEN);
+	const eventsPath = path.join(__dirname, 'events');
+	const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+
+	for (const file of eventFiles) {
+		const filePath = path.join(eventsPath, file);
+		const event = require(filePath);
+		if (event.once) {
+			client.once(event.name, (...args) => event.execute(...args));
+		}
+		else {
+			client.on(event.name, (...args) => event.execute(...args));
+		}
+	}
+
+	client.login(process.env.PRODTOKEN);
+};
---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express'),
+	app = express();
+
+app.get('/', function (req, res) {
+	res.sendStatus(200);
+	const ping = new Date();
+	ping.setHours(ping.getHours() - 3);
+	console.log(
+		`Ping recebido às ${ping.getUTCHours()}:${ping.getUTCMinutes()}:${ping.getUTCSeconds()}`,
+	);
+});
+
+require(__dirname + '/bot.js')(app);
+
+const listener = app.listen(process.env.PORT || 3000, function () {
+	console.log(`Porta: ${listener.address().port}`);
+});
+
+module.exports = app;
